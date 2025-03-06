4afaee6ea17028964dba43dbfee91b1e19ee20ef
crwn-up <luidran1@gmail.com>
2025-03-06 14:04:03 -0600

    Create ejercicio1.c

---
+++ ejercicio1.c
@@ -0,0 +1,14 @@
+// Luis Crown
+//Ejercicio 1
+# include <stdio.h> 
+# include <stdlib.h>
+int main ()
+{
+    int a= 3;
+    int b= 2;
+    if (a<b)
+    {
+        printf ("a es mayor h \n");
+    }
+    return 0;
+}
