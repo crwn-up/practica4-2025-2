cb7e8d046221e43b1819772ec7f5fb7e4db78023
crwn-up <luidran1@gmail.com>
2025-03-06 14:17:16 -0600

    Create ejercicio2.c

---
+++ ejercicio2.c
@@ -0,0 +1,18 @@
+// Luis Crown
+//Ejercicio 1
+# include <stdio.h> 
+# include <stdlib.h>
+int main ()
+{
+    int a= 3;
+    int b= 5;
+    if (a>b)
+    {
+        printf ("a es mayor h \n");
+    }
+    else
+    {
+        printf ("b es mayor o igual a \n");
+    }
+    return 0;
+}
