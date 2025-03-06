74a01bea0cdb0baa510066ac52fe8fdb30cf2c34
crwn-up <luidran1@gmail.com>
2025-03-06 14:44:37 -0600

    Create ejercicio 3.c

---
+++ ejercicio 3.c
@@ -0,0 +1,36 @@
+//Luis Crown
+//Ejercicio suma
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int res= 0;
+    int i;
+
+    for(i = 0; i < 11; i++)
+     {   
+         res = res + i;
+     }
+    printf("Solución 1 - El numero es: %d\n",res);
+}
+ res= 0;
+ for(i = 1; i <= 10; i++)
+     {   
+         res = res + i;
+     }
+    printf("Solución 2 - El numero es: %d\n",res);
+}
+res= 0;
+ for(i = 0; i <= 10; i++)
+     {   
+         res = res + i;
+     }
+    printf("Solución 3 - El numero es: %d\n",res);
+}
+res= 0;
+ for(i = 1; i < 11; i++)
+     {   
+         res = res + i;
+     }
+    printf("Solución 4 - El numero es: %d\n",res);
+}
