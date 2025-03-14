f1a6dab87d762a24a02cbbfcee6f770954b96a17
crwn-up <luidran1@gmail.com>
2025-03-14 00:39:45 -0600

    Update ejercicio 3.c

--- ejercicio 3.c
+++ ejercicio 3.c
@@ -1,36 +1,44 @@
-//Luis Crown
-//Ejercicio suma
+// Luis Crown
+// Ejercicio suma
 #include <stdio.h>
 #include <stdlib.h>
-int main()
-{
-    int res= 0;
+
+int main() {
+    int res = 0;
     int i;
 
-    for(i = 0; i < 11; i++)
-     {   
-         res = res + i;
-     }
-    printf("Solución 1 - El numero es: %d\n",res);
-}
- res= 0;
- for(i = 1; i <= 10; i++)
-     {   
-         res = res + i;
-     }
-    printf("Solución 2 - El numero es: %d\n",res);
-}
-res= 0;
- for(i = 0; i <= 10; i++)
-     {   
-         res = res + i;
-     }
-    printf("Solución 3 - El numero es: %d\n",res);
-}
-res= 0;
- for(i = 1; i < 11; i++)
-     {   
-         res = res + i;
-     }
-    printf("Solución 4 - El numero es: %d\n",res);
+    // Solución 1
+    for (i = 0; i < 11; i++) {   
+        res = res + i;
+    }
+    printf("Solución 1 - El número es: %d\n", res);
+
+    // Reiniciar res para la siguiente solución
+    res = 0;
+
+    // Solución 2
+    for (i = 1; i <= 10; i++) {   
+        res = res + i;
+    }
+    printf("Solución 2 - El número es: %d\n", res);
+
+    // Reiniciar res para la siguiente solución
+    res = 0;
+
+    // Solución 3
+    for (i = 0; i <= 10; i++) {   
+        res = res + i;
+    }
+    printf("Solución 3 - El número es: %d\n", res);
+
+    // Reiniciar res para la siguiente solución
+    res = 0;
+
+    // Solución 4
+    for (i = 1; i < 11; i++) {   
+        res = res + i;
+    }
+    printf("Solución 4 - El número es: %d\n", res);
+
+    return 0;
 }
